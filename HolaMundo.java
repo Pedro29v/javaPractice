011c309d0c9bf74a5348447f823c6234014260f6
Pedro <pedrocastillo24.pc@gmail.com>
2023-08-21 19:10:41 -0300

    mi first HELLO WORLD in java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 class HolaMundo {
 
-    public static void main(string arg[]){
+    public static void main(String arg[]){
 
-        System.out.println(x:"Hello World!");
+        System.out.println("Hello World!");
     }
 }
