22c67bef6329bd5170ec59dd5611b265120a7d88
Pedro <pedrocastillo24.pc@gmail.com>
2023-08-22 20:38:34 -0300

    concatenates variable

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,9 @@ class holaMundo {
 
     public static void main(String arg[]){
 
+        int age = 31;
+
         System.out.println("Hello World!");
-        System.out.print("My name is Pedro");
+        System.out.print("My name is Pedro and I´m " + age + " years old");
     }
 }
